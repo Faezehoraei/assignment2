daaf55c86bb28c176c5051c32a9ff55be992c93c
Faezehoraei <92444156+Faezehoraei@users.noreply.github.com>
2021-10-13 18:59:56 +0330

    Add files via upload

---
+++ convert_time _to_seconds.py
@@ -0,0 +1,10 @@
+# In The Name Of GOD
+# Programmer is : Faezeh Oraei MiriNejad
+# ID is : 96440164
+
+print("Please enter the time = ")
+hour = int(input('Hour = '))
+minute = int(input('Minute = '))
+second = int(input('Second = '))
+sec = hour*3600 + minute*60 + second
+print('Convert time to seconds = ', sec)
